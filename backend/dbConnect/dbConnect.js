5b4689a9862107cf5b1a53ff6eedbe1831ae17d9
RvMern <rkv51508@gmail.com>
2023-11-07 10:33:15 +0530

    first commit

--- backend/dbConnect/dbConnect.js
+++ backend/dbConnect/dbConnect.js
@@ -2,7 +2,7 @@ const mongoose = require("mongoose");
 
 // Database Connection
 const dbConnect = async()=>{
-  await mongoose.connect(process.env.MONGODB_URL,{useNewUrlParser:true, useUnifiedTopology:true})
+  await mongoose.connect(process.env.MONGODB_ATLAS_URL,{useNewUrlParser:true, useUnifiedTopology:true})
    .then((data)=>{console.log(`Database Connected Successfully with server ${data.connection.host}`)})
 }
 
